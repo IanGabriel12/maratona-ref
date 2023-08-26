dd834aaa0cb4246d340259d56fee252444a92c80
Pablo Gustavo <34045117+Pablo1Gustavo@users.noreply.github.com>
2023-08-26 20:19:46 -0300

    Update sieve func starting is_prime and add a N parameter.

--- code/math/sieve.cpp
+++ code/math/sieve.cpp
@@ -1,11 +1,13 @@
 // Crivo de erastótenes
 bool is_prime[MAX];
 vector<int> primes;
-void sieve() {
-  for(int i=2; i<=SQRT_MAX; i++) {
+void sieve(int n) {
+  memset(is_prime, 1, n + 1);
+  is_prime[0] = is_prime[1] = false;
+  for(int i=2; i<=n; i++) {
     if(!is_prime[i]) continue;
     primes.push_back(i);
-    for(int j=2*i; j<=MAX; j+=i) {
+    for(int j=i*i; j<=n; j+=i) {
       is_prime[j] = false;
     }
   }
