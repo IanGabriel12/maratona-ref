b02ba3bb5caa3c12a3b71d2164a21e3f68f6471d
Pablo Gustavo <34045117+Pablo1Gustavo@users.noreply.github.com>
2023-09-04 13:49:10 -0300

    Update floyd_warshaall.cpp

--- code/graphs/floyd_warshaall.cpp
+++ code/graphs/floyd_warshaall.cpp
@@ -2,6 +2,13 @@
 // Floyd-Warshall O(n^3)
 // (all shortest paths between nodes in single run)
 void floyd_warshall() {
+  for (int i=1; i<=n; i++) {
+    for (int j=1; j<=n; j++) {
+      if (i == j) distance[i][j] = 0;
+      else if (adj[i][j]) distance[i][j] = adj[i][j];
+      else distance[i][j] = INF;
+    }
+  }
   for (int k=1; k<=n; k++) {
     for (int i=1; i<=n; i++) {
       for (int j=1; j<=n; j++) {
