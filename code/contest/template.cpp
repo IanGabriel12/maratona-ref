8b1f6d502b7f0cd014ce57bb86a9611950757401
Ian Gabriel <50637958+IanGabriel12@users.noreply.github.com>
2026-04-19 10:16:35 -0300

    Update template

--- code/contest/template.cpp
+++ code/contest/template.cpp
@@ -1,12 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define int long long
 #define rep(i, a, b) for(int i=a; i<(b); i++)
 #define all(x) x.begin(), x.end()
 #define _ ios_base::sync_with_stdio(false);cin.tie(NULL);
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
 
 int main() {_
     
